6172a4c66123e0fec35c804293401884ea19f1ea
Estefania Morton <estefania.morton@gmail.com>
2017-01-13 10:16:05 -1000

    dddonnneee

--- todo_app.sql
+++ todo_app.sql
@@ -30,7 +30,27 @@ INSERT INTO tasks VALUES (DEFAULT, 'Study SQL', 'Complete this excercise', now()
 
 INSERT INTO tasks VALUES (DEFAULT, 'Study PostgreSQL', 'Read all the documentation', now(), now(), NULL);
 
-SELECT title FROM tasks WHERE completed_at = NULL;
+SELECT title FROM tasks WHERE completed_at IS NULL;
 
-UPDATE tasks SET completed_at = now() WHERE title = 'Study SQL'
+UPDATE tasks SET completed_at = now() WHERE title = 'Study SQL';
+
+SELECT title, description FROM tasks WHERE completed_at IS NULL;
+
+SELECT * FROM tasks ORDER BY created_at ASC;
+
+INSERT INTO tasks VALUES (DEFAULT, 'mistake 1', 'a test entry', now(), now(), NULL);
+
+INSERT INTO tasks VALUES (DEFAULT, 'mistake 2', 'another test entry', now(), now(), NULL);
+
+INSERT INTO tasks VALUES (DEFAULT, 'third mistake', 'another test entry', now(), now(), NULL);
+
+SELECT title FROM tasks WHERE title LIKE '%mistake%';
+
+DELETE FROM tasks where title = 'mistake 1';
+
+SELECT title, description FROM tasks WHERE title LIKE '%mistake%';
+
+DELETE FROM tasks where title LIKE '%mistake%';
+
+SELECT * FROM tasks ORDER BY title ASC;
 \dt;
